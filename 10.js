baf99db6a28e5061e77777d66af45a12927437b0
samurai20000 <samurai20000@gmail.com>
2015-09-01 21:13:11 +0200

    Add 10.js

---
+++ 10.js
@@ -0,0 +1,19 @@
+var json = {
+    "name": {
+        "first": "Yosuke",
+        "family": process.argv[2]
+    },
+    "birth": {
+        "year": 1982,
+        "month": 12,
+        "day": process.argv[3]
+    }
+};
+// ここでデストラクチャリングを使ってname.familyをfamilyNameに。
+// birth.day を birthDay に束縛してください。
+
+var {name:{family: familyName}, birth: {day: birthDay}} = json;
+
+
+console.log(familyName);
+console.log(birthDay);
